e62087ab73cda9a2ff6ecb14ce8b0077f252c34d
Steve <30637170+overallcoma@users.noreply.github.com>
2023-01-17 16:18:32 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,17 @@
 
 export C2_FILE="/hak5c2/c2-3.2.0_amd64_linux"
 echo "${C2_FILE}"
+echo "${C2_HOSTNAME}"
 ls /hak5c2
-ls /
 
 export C2_SWITCHES="-reverseProxy"
 
-if [ -z ${C2_DEBUG} ]
+if [ -z ${C2_DEBUG+x} ]
 then
     export C2_SWITCHES="${C2_SWITCHES} -debug"
 fi
 
-if [ -z ${C2_HOSTNAME} ]
+if [ -z ${C2_HOSTNAME+x} ]
 then
     export C2_SWITCHES="${C2_SWITCHES} -hostname ${C2_HOSTNAME}"
 else
@@ -21,7 +21,7 @@ else
     init 0
 fi
 
-if [ -z ${C2_LICENSE_KEY} ]
+if [ -z ${C2_LICENSE_KEY+x} ]
 then
     export C2_SWITCHES="${C2_SWITCHES} -setLicenseKey ${C2_LICENSE_KEY}"
 fi
