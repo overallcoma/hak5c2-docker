a0c25fdf576f8aba0ed598fcc4b3ba97f0c8b4ef
Steve <30637170+overallcoma@users.noreply.github.com>
2023-01-17 15:20:48 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 export C2_FILE="$(find /hak5c2/ -name '*amd64_linux')"
-export C2_SWITCHES=""
 
-export C2_SWITCHES = "${C2_SWITCHES} -reverseProxy"
+export C2_SWITCHES="${C2_SWITCHES} -reverseProxy"
 
 if test -z "$C2_DEBUG"
 then
-    export C2_SWITCHES = "${C2_SWITCHES} -debug"
+    export C2_SWITCHES="${C2_SWITCHES} -debug"
 fi
 
 if test -z "$C2_HOSTNAME"
 then
-    export C2_SWITCHES = "${C2_SWITCHES} -hostname ${C2_HOSTNAME}"
+    export C2_SWITCHES="${C2_SWITCHES} -hostname ${C2_HOSTNAME}"
 else
     print "C2_HOSTNAME is a required variable"
     sleep 10
@@ -21,7 +20,7 @@ fi
 
 if test -z "$C2_LICENSE_KEY"
 then
-    export C2_SWITCHES = "${C2_SWITCHES} -setLicenseKey ${C2_LICENSE_KEY}
+    export C2_SWITCHES="${C2_SWITCHES} -setLicenseKey ${C2_LICENSE_KEY}
 fi
 
 /hak5c2/${C2_FILE} ${C2_SWITCHES}
