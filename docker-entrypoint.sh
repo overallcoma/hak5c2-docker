fc7d82fa68565a5458843830c121099c4dca046d
Steve <30637170+overallcoma@users.noreply.github.com>
2023-01-18 11:04:51 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export C2_FILE="/hak5c2/c2-3.2.0_amd64_linux"
+C2_FILE="/hak5c2/c2-3.2.0_amd64_linux"
 echo "${C2_FILE}"
 chmod +x "${C2_FILE}"
 echo "${C2_FILE}"
 echo "${C2_HOSTNAME}"
-ls /hak5c2
+C2_SWITCHES=()
 
 if [ -z ${C2_HOSTNAME+x} ]
 then
@@ -14,29 +14,30 @@ then
     exit
 else
     C2_SWITCHES+=( "-hostname" )
-    C2_SWITCHES+=(  "${C2_HOSTNAME}" )
+    C2_SWITCHES+=( $C2_HOSTNAME )
 fi
 
-C2_SWITCHES+=( "-reverseProxy" )
-C2_SWITCHES+=( "-reverseProxyPort 4242" )
+C2_SWITCHES+=("-reverseProxy")
+C2_SWITCHES+=("-reverseProxyPort")
+C2_SWITCHES+= ("4242")
 
 if [ -z ${C2_DEBUG+x} ]
 then
     echo "Debug not set"
 else
-    C2_SWITCHES+=( "-debug" )
+    C2_SWITCHES+=("-debug")
 fi
 
 if [ -z ${C2_LICENSE_KEY+x} ]
 then
     echo "License Key Not Set"
 else
-    C2_SWITCHES+=( "-setLicenseKey" )
-    C2_SWITCHES+=( "${C2_LICENSE_KEY}" )
+    C2_SWITCHES+=("-setLicenseKey")
+    C2_SWITCHES+=($C2_LICENSE_KEY)
 fi
 
-for i in ${C2_SWITCHES}; do
+for i in $C2_SWITCHES; do
     echo $i
 done
 
-"${C2_FILE}" "${C2_SWITCHES}"
+$C2_FILE $C2_SWITCHES
